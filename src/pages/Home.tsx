38d8864dd60ca9108436542ae013a9540a2d1354
Lucas da Silva <lucasdasilva68@yahoo.com>
2021-06-20 16:42:59 -0300

    add new task, toggle task completion, remove task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,30 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    } as Task;
+
+    if(newTaskTitle.length !== 0) {
+      setTasks(oldState => [...oldState, data ])
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    setTasks(oldState => oldState.map(
+      task => task.id === id ? {...task, done: !task.done} : task      
+      ));
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ));
   }
 
   return (
